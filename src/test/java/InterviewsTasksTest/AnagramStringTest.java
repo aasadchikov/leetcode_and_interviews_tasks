0da69da8f9f37223b5b3e9b6959ee0dac6149e80
aasadchikov <aasadchikov@gmail.com>
2022-07-05 12:18:34 -0700

    updates by AnagramTest

--- src/test/java/InterviewsTasksTest/AnagramStringTest.java
+++ src/test/java/InterviewsTasksTest/AnagramStringTest.java
@@ -1,2 +1,23 @@
-package InterviewsTasksTest;public class AnagramStringTest {
+package InterviewsTasksTest;
+
+import InerviewsTasks.AnagramString;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AnagramStringTest {
+
+    @Test
+    public void testAnagram1() {
+
+        Assert.assertEquals(AnagramString.isAnagram("Keep", "Peek"), true);
+
+    }
+
+    @Test
+    public void testAnagram2() {
+
+        Assert.assertEquals(AnagramString.isAnagram("Mother In Law", "Hitler Woman"), true);
+
+    }
+
 }
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class MainTest {
+import org.apache.commons.lang3.StringUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void testAbbr4() {
+        String result = StringUtils.abbreviate("Anton Sadchikov", 4);
+        Assert.assertEquals(result, "A...");
+    }
+
+    @Test
+    public void testAbbr8() {
+        String result = StringUtils.abbreviate("Hello world", 8);
+        Assert.assertEquals(result, "Hello...");
+    }
 }
