5dba9cd291c593d0b1450ae0773c641342084d3e
aasadchikov <aasadchikov@gmail.com>
2022-07-06 11:45:03 -0700

    ReverseString task added

---
+++ src/test/java/InterviewsTasksTest/ReverseStringTest.java
@@ -0,0 +1,16 @@
+package InterviewsTasksTest;
+
+import LeetCode.ReverseStringSolution;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ReverseStringTest {
+
+    @Test
+    public void testReverseString() {
+
+        Assert.assertEquals(ReverseStringSolution.reverseString(new char[]{'h', 'e', 'l', 'l', 'o'}), new char[]{'o', 'l', 'l', 'e', 'h'});
+
+    }
+}
+
