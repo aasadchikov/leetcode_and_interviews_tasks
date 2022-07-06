c37e9c938aaa6f19e72464e509f4b81f7c9eefb2
aasadchikov <aasadchikov@gmail.com>
2022-07-05 22:32:38 -0700

    Anagram task added

--- src/main/java/InerviewsTasks/AnagramString.java
+++ src/main/java/InerviewsTasks/AnagramString.java
@@ -4,19 +4,22 @@ import java.util.Arrays;
 
 public class AnagramString {
 
-    public static Boolean isAnagram(String str1, String str2) {
-        String s1 = str1.replaceAll("\\s", "");
-        String s2 = str2.replaceAll("\\s", "");
-        boolean status = true;
-        if (s1.length() != s2.length()) {
-            status = false;
-        } else {
-            char[] ArrayS1 = s1.toLowerCase().toCharArray();
-            char[] ArrayS2 = s2.toLowerCase().toCharArray();
-            Arrays.sort(ArrayS1);
-            Arrays.sort(ArrayS2);
-            status = Arrays.equals(ArrayS1, ArrayS2);
+    public static Boolean isAnagram(String s1, String s2) {
+        Boolean result = false;
+
+        String str1 = s1.replaceAll("\\s", "");
+        String str2 = s2.replaceAll("\\s", "");
+
+        if (str1.length() == str2.length()) {
+            char[] arrayS1 = str1.toLowerCase().toCharArray();
+            char[] arrayS2 = str2.toLowerCase().toCharArray();
+
+            Arrays.sort(arrayS1);
+            Arrays.sort(arrayS2);
+
+            result = Arrays.equals(arrayS1, arrayS2);
         }
-        return status;
+
+        return result;
     }
 }
