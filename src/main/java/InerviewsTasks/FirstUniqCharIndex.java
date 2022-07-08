dfb8f4de8ff6319d6477bfaed690eda662300616
aasadchikov <aasadchikov@gmail.com>
2022-07-07 23:53:47 -0700

    "First Unique Character in a String" task added

---
+++ src/main/java/InerviewsTasks/FirstUniqCharIndex.java
@@ -0,0 +1,31 @@
+package InerviewsTasks;
+
+import java.util.HashMap;
+
+public class FirstUniqCharIndex {
+
+    public static int firstUniqChar(String s) {
+        //1st step Count of all characters in the string
+        //2nd step to compare input string characters to our key/value pais
+
+        HashMap<Character, Integer> countMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char letter = s.charAt(i);
+            if (countMap.containsKey(letter)) {
+                countMap.put(letter, countMap.get(letter) + 1);
+            } else {
+                countMap.put(letter, 1);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            char letter = s.charAt(i);
+            if (countMap.get(letter) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
