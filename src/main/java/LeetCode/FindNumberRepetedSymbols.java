cbbb89fff5cf72000604809e734c170fedc3cff0
aasadchikov <aasadchikov@gmail.com>
2022-08-02 17:40:41 -0700

    tasks added

--- src/main/java/LeetCode/FindNumberRepetedSymbols.java
+++ src/main/java/LeetCode/FindNumberRepetedSymbols.java
@@ -28,10 +28,35 @@ public class FindNumberRepetedSymbols {
 
     }
 
+    public static void countDuplicatedDigits(int[] arr){
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num : arr) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            }
+            else {
+                map.put(num, 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " : " + entry.getValue());
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         String str = "geeksforgeeks";
 
         // Function Call
         countDuplicatedCharacters(str);
+
+        int[] arr = {1,2,3,3,3,3,4,5,6,6,6,7,8};
+        countDuplicatedDigits(arr);
+
     }
 }
--- src/test/java/LeetCodeTest/TwoSumTest.java
+++ src/test/java/LeetCodeTest/TwoSumTest.java
@@ -8,12 +8,17 @@ public class TwoSumTest {
 
     @Test
     public void testTwoSum() {
-        Assert.assertEquals(TwoSum.twoSum(new int[]{2, 7, 11, 15}, 9), new int[]{0, 1});
+        Assert.assertEquals(TwoSum.twoSum(new int[]{2, 7, 7, 15}, 14), new int[]{1, 2});
     }
 
     @Test
     public void testTwoSumTwoPassHashTable() {
         Assert.assertEquals(TwoSum.twoSumTwoPassHashTable(new int[]{2, 7, 11, 15}, 9), new int[]{0, 1});
+    }
+
+    @Test
+    public void testTwoSumTwoPassHashTable1() {
+        Assert.assertEquals(TwoSum.twoSumTwoPassHashTable(new int[]{2, 2, 7, 15}, 4), new int[]{0, 1});
     }
 
     @Test
